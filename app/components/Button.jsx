8e750b47d52544b4f8346e17c2ecebceae693408
Akash Kumar <codewithpanda28@gmail.com>
2024-11-14 21:05:07 +0530

    adding navbar and hero section

---
+++ app/components/Button.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+
+const Button = ({type, title, icon, variant}) => {
+  return (
+ <button className={`flexCenter gap-3 rounded-full border ${variant}`} type={type} >
+ {icon && <Image src={icon} alt={title} width={24} height={24}/>}
+ <label className="bold-16 whitespace-nowrap">{title}</label>
+ </button>
+  )
+}
+
+export default Button;
